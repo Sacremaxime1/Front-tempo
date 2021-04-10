d91a74afd94c1db6c27bdd80a6da35251ae07357
Adrien Crespel <a.crespel56@gmail.com>
2021-04-10 02:38:40 +0200

    add routes

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -5,13 +5,19 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
+import Navbar from "../Navbar";
 import Home from "../../pages/Home";
+import Cards from "../../pages/Cards";
+import Equipments from "../../pages/Equipments";
 
 const index = () => {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/cartes" exact component={Cards} />
+        <Route path="/equipements" exact component={Equipments} />
         <Redirect to="/" />
       </Switch>
     </Router>
