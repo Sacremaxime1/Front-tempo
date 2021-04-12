dad407cee72e4c78f93f5b21e2220d6571520ee5
Maxime Bompoil <maxime.bompoil@gmail.com>
2021-04-12 15:42:43 +0200

    basic search bar

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { useSelector } from "react-redux";
 import CardsList from "../components/CardsList";
 import FormCard from "../components/FormCard";
+import { FormInput } from "../components/FormCard/FormCardElements";
 
 const Cards = () => {
   const cards = useSelector((state) => state.cardReducer);
+  const [searchTerm, setSearchTerm] = useState("");
+  const updateSearchTerm = useCallback((event) =>
+    setSearchTerm(event.target.value)
+  );
 
   return (
-  <>
-  	<FormCard />
-  	<CardsList cards={cards}></CardsList>
-  </>
+    <>
+      <FormCard />
+      <FormInput type="text" onChange={updateSearchTerm}></FormInput>
+      <CardsList
+        cards={cards.filter((card) =>
+          card.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )}
+      ></CardsList>
+    </>
   );
 };
 
